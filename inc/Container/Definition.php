73bdc3bd8f661be5d60ce363c32e0e18207aa6ee
COQUARD Cyrille <coquardcyr@gmail.com>
2024-08-10 00:41:46 +0200

    Fixed a bug with the definition

---
+++ inc/Container/Definition.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace LaunchpadCore\Container;
+
+use League\Container\Argument\ArgumentInterface;
+use League\Container\Argument\ClassNameInterface;
+use League\Container\Argument\LiteralArgumentInterface;
+use League\Container\Argument\RawArgumentInterface;
+use League\Container\Exception\ContainerException;
+use Psr\Container\ContainerInterface;
+
+class Definition extends \League\Container\Definition\Definition {
+	/**
+	 * {@inheritdoc}
+	 */
+	public function resolve( bool $new = false ) {
+		$concrete = $this->concrete;
+
+		if ( $this->isShared() && $this->resolved !== null && $new === false ) {
+			return $this->resolved;
+		}
+
+		if ( is_callable( $concrete ) ) {
+			$concrete = $this->resolveCallable( $concrete );
+		}
+
+		if ( $concrete instanceof RawArgumentInterface ) {
+			$this->resolved = $concrete->getValue();
+
+			return $concrete->getValue();
+		}
+
+		if ( $concrete instanceof ClassNameInterface ) {
+			$concrete = $concrete->getClassName();
+		}
+
+		if ( is_string( $concrete ) ) {
+			if ( $this->alias !== $this->concrete && is_string( $concrete ) && $this->getContainer()->has( $concrete ) ) {
+				$concrete = $this->getContainer()->get( $concrete );
+			}
+			elseif ( class_exists( $concrete ) ) {
+					$concrete = $this->resolveClass( $concrete );
+			}
+		}
+
+		if ( is_object( $concrete ) ) {
+			$concrete = $this->invokeMethods( $concrete );
+		}
+
+		$this->resolved = $concrete;
+
+		return $concrete;
+	}
+}
