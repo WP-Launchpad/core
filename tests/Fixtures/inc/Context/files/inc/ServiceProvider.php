5025e0b4417a7995059666051cf6e6474f19cf8d
COQUARD Cyrille <coquardcyr@gmail.com>
2024-09-03 20:22:18 +0200

    Added tests

---
+++ tests/Fixtures/inc/Context/files/inc/ServiceProvider.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace LaunchpadCore\Tests\Fixtures\inc\Context\files\inc;
+
+use LaunchpadCore\Container\AbstractServiceProvider;
+
+class ServiceProvider extends AbstractServiceProvider {
+
+	/**
+	 * @inheritDoc
+	 */
+	protected function define() {
+		$this->register_common_subscriber(Subscriber::class);
+		$this->register_common_subscriber(ClassContextSubscriber::class);
+	}
+}
